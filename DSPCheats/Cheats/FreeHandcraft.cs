2dfceabcd54cf97f43d1346423657e379f6c44d4
Aaron Robinson <aaronrobin1234@gmail.com>
2021-02-20 00:10:23 -0600

    Fix FreeHandcrafts manipulation of the Handcraft bool persisting loads

--- DSPCheats/Cheats/FreeHandcraft.cs
+++ DSPCheats/Cheats/FreeHandcraft.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using HarmonyLib;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
@@ -16,17 +15,38 @@ namespace DSPCheats.Cheats
         {
             ManualApply = true
         });
+        private ILHook _okButtonHook = new ILHook(typeof(UIReplicatorWindow).GetMethod("OnOkButtonClick", BindingFlags.NonPublic | BindingFlags.Instance), new ILContext.Manipulator(OkButtonHandcraftILHook), new ILHookConfig
+        {
+            ManualApply = true
+        });
+        private ILHook _refreshIconsHook = new ILHook(typeof(UIReplicatorWindow).GetMethod("RefreshRecipeIcons", BindingFlags.NonPublic | BindingFlags.Instance), new ILContext.Manipulator(RefreshIconsILHook), new ILHookConfig
+        {
+            ManualApply = true
+        });
+        private ILHook _replicatorWindowUpdateHook = new ILHook(typeof(UIReplicatorWindow).GetMethod("_OnUpdate", BindingFlags.NonPublic | BindingFlags.Instance), new ILContext.Manipulator(ReplicatorUpdateILHook), new ILHookConfig
+        {
+            ManualApply = true
+        });
         private bool _currentlyHooked = false;
 
         public override void ChangeState(bool enable)
         {
-            _newGame = true;
             if (enable != _currentlyHooked)
             {
                 if (enable)
+                {
                     _takeItemsHook.Apply();
+                    _okButtonHook.Apply();
+                    _refreshIconsHook.Apply();
+                    _replicatorWindowUpdateHook.Apply();
+                }
                 else
+                {
                     _takeItemsHook.Undo();
+                    _okButtonHook.Undo();
+                    _refreshIconsHook.Undo();
+                    _replicatorWindowUpdateHook.Undo();
+                }
                 _currentlyHooked = enable;
             }
         }
@@ -47,28 +67,6 @@ namespace DSPCheats.Cheats
             return false;
         }
 
-        private static bool _newGame = true;
-        private static bool _lastItemHandcraft = false;
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(UIReplicatorWindow), "SetSelectedRecipeIndex")]
-        public static void MakeAllRecipesAvailablePrefix(ref UIReplicatorWindow __instance, ref int index)
-        {
-            if (__instance.selectedRecipe is not null) {
-                __instance.selectedRecipe.Handcraft = _newGame ? __instance.selectedRecipe.Handcraft : _lastItemHandcraft;
-            }
-            _lastItemHandcraft = LDB.recipes.Select(index)?.Handcraft ?? false;
-            _newGame = false;
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(UIReplicatorWindow), "SetSelectedRecipeIndex")]
-        public static void MakeAllRecipesAvailablePostfix(ref UIReplicatorWindow __instance)
-        {
-            if (__instance.selectedRecipe is not null)
-                __instance.selectedRecipe.Handcraft = true;
-        }
-
         public static void DontTakeItemsILHook(ILContext il)
         {
             ILCursor c = new ILCursor(il);
@@ -87,6 +85,58 @@ namespace DSPCheats.Cheats
 
             c.RemoveRange(6);
             c.Emit(OpCodes.Ldloc, count2loc);
+        }
+
+        public static void OkButtonHandcraftILHook(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            ILLabel placeToSkipTo = null;
+
+            c.TryGotoNext(MoveType.Before,
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld(typeof(UIReplicatorWindow).GetField("selectedRecipe", BindingFlags.NonPublic | BindingFlags.Instance)),
+                x => x.MatchLdfld(typeof(RecipeProto).GetField("Handcraft", BindingFlags.Public | BindingFlags.Instance)),
+                x => x.MatchBrtrue(out placeToSkipTo)
+            );
+
+            c.Emit(OpCodes.Br, placeToSkipTo);
+        }
+
+        public static void RefreshIconsILHook(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            int handcraftLoc = 7;
+
+            c.GotoNext(MoveType.After,
+                x => x.MatchLdloc(1),
+                x => x.MatchLdloc(3),
+                x => x.MatchLdelemRef(),
+                x => x.MatchLdfld(typeof(RecipeProto).GetField("Handcraft", BindingFlags.Public | BindingFlags.Instance)),
+                x => x.MatchStloc(out handcraftLoc)
+            );
+
+            c.Emit(OpCodes.Ldc_I4_1);
+            c.Emit(OpCodes.Stloc, handcraftLoc);
+        }
+
+        public static void ReplicatorUpdateILHook(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            c.GotoNext(MoveType.Before,
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld(typeof(UIReplicatorWindow).GetField("selectedRecipe", BindingFlags.NonPublic | BindingFlags.Instance)),
+                x => x.MatchBrfalse(out _),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld(typeof(UIReplicatorWindow).GetField("selectedRecipe", BindingFlags.NonPublic | BindingFlags.Instance)),
+                x => x.MatchLdfld(typeof(RecipeProto).GetField("Handcraft", BindingFlags.Public | BindingFlags.Instance)),
+                x => x.MatchBrfalse(out _)
+            );
+
+            c.Index += 3;
+            c.RemoveRange(4);
         }
     }
 }
